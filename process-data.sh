ccb25469c747025d866efafefd12d923eb8f464d
Rob Moss <romoss@cisco.com>
2025-12-10 14:23:32 +1100

    Pulled data from 2025-12-10 Dec 10 2025

--- process-data.sh
+++ process-data.sh
@@ -9,3 +9,7 @@ jsondir=./jsondata
 ./json_to_markdown.py $jsondir --out $datadir
 
 pandoc "${datadir}/Cisco UCS Manager Cross-Version Firmware Support, Release 4.3 - Cisco.html" -f html -t markdown -o ${datadir}/cross-version-firmware-4.3.md
+
+./extract_adapter_matrix.py
+./validate_firmware_data.py > validation_report.md
+
