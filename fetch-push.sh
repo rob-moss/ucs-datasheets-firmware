6992bc6d478a7e20abcd873572112ff33365ed87
Rob Moss <romoss@cisco.com>
2026-06-30 16:34:40 +1000

    Updates 30 Jun 26

---
+++ fetch-push.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source .venv/bin/activate
+
+# fetch-cisco
+python3 fetch-cisco-docs.py
+if [ $? -ne 0 ]; then
+    echo "Error fetching Cisco docs"
+    exit 1
+fi
+
+# Fetch recommended
+cd recommended-firmware
+python3 fetch-imm.py
+if [ $? -ne 0 ]; then
+    echo "Error fetching recommended firmware"
+    exit 1
+fi
+python3 fetch-ucsm.py
+if [ $? -ne 0 ]; then
+    echo "Error fetching recommended firmware"
+    exit 1
+fi
+
+git add ucs-docs/*
+git add recommended-firmware/*
+git commit -m "Updates $(date +'%d %b %y')"
+git push
+
+
